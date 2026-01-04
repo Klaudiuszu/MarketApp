509a0727f70d9457cbcc5cbf06d764e4bfb8b476
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2026-01-04 13:21:33 +0100

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
   title: "Assets for Fund Managers",
   description:
-    "Track real-time stock market data and make informed investment decisions with Investment intent.",
+    "Track real-time stock market data and make informed investment decisions.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         {children}
         <Toaster />
       </body>
